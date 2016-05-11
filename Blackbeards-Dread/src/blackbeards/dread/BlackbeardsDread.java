88d9208cc37539b970b85b00685d531cbd1b5253
jkbry <jkbry@DESKTOP-Q7K9TD8>
2016-05-11 10:17:34 -0600

    5/11/16 (10:17am) - BlackbeardsDread main file commit

--- Blackbeards-Dread/src/blackbeards/dread/BlackbeardsDread.java
+++ Blackbeards-Dread/src/blackbeards/dread/BlackbeardsDread.java
@@ -5,7 +5,12 @@
  */
 package blackbeards.dread;
 
+import blackBeardsDread.model.Battle;
 import blackBeardsDread.model.Game;
+import blackBeardsDread.model.Location;
+import blackBeardsDread.model.Map;
+import blackBeardsDread.model.Sea;
+import blackBeardsDread.model.Ship;
 
 /**
  *
@@ -18,14 +23,53 @@ public class BlackbeardsDread {
      */
     public static void main(String[] args) {
         Game game1 = new Game();
+            game1.setPlayerName("Joe");
+            game1.setSaveShip(12);
+            game1.setSaveCurrentLocation(2);
         
-        game1.setPlayerName("Joe");
-        game1.setSaveShip(12);
-        game1.setSaveCurrentLocation(2);
-        
-        String gameInfo = game1.toString();
-        System.out.println(gameInfo);
+            String gameInfo = game1.toString();
+            System.out.println(gameInfo);
     
+        Battle battle1 = new Battle();
+            battle1.setShipDamage(10);
+            battle1.setReward(2);
+            battle1.setShipDifficulty(2);
+            battle1.setEscape(1);
+        
+            String battleInfo = battle1.toString();
+            System.out.println(battleInfo);
+            
+        Location location1 = new Location();
+            location1.setPortName("Canada");
+            location1.setWaterStore(2);
+            location1.setGoodStore(2);
+            location1.setArmory(5);
+            location1.setDryDock(7);
+            location1.setGuildHall(10);
+            
+            String locationInfo = location1.toString();
+            System.out.println(locationInfo);
+            
+        Ship ship1 = new Ship();
+            ship1.setHealth(10);
+            ship1.setDamage(6);
+            ship1.setWeapons(2);
+            ship1.setSails(3);
+            
+            String shipInfo = ship1.toString();
+            System.out.println(shipInfo);
+    
+        Map map1 = new Map();
+            map1.setCurrentLocation("London");
+            
+            String mapInfo = map1.toString();
+            System.out.println(mapInfo);
+    
+        Sea sea1 = new Sea();
+            sea1.setStartLocation("London");
+            sea1.setEndLocation("Spain");
+            
+            String seaInfo = sea1.toString();
+            System.out.println(seaInfo);
     }
-    
 }

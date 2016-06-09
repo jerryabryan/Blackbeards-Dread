1a1fc37b2a3e053389dee5ac61481d233765ba1f
jkbry <jkbry@DESKTOP-Q7K9TD8>
2016-06-09 09:19:19 -0600

    6/9/16 - Group Presentation Meeting (Only Removed "$" from instances)

--- Blackbeards-Dread/src/blackBeardsDread/Control/PortControl.java
+++ Blackbeards-Dread/src/blackBeardsDread/Control/PortControl.java
@@ -10,58 +10,58 @@ package blackBeardsDread.Control;
  * @author jkbry
  */
 public class PortControl {
-    public double purchaseWater(double $water, double $gold, double $desiredWater, double $waterPrice) {
-        if ($desiredWater < 0 || $desiredWater > 10){
+    public double purchaseWater(double water, double gold, double desiredWater, double waterPrice) {
+        if (desiredWater < 0 || desiredWater > 10){
             return -1;
         }
         
-        if ($gold < ($desiredWater * $waterPrice)) {
+        if (gold < (desiredWater * waterPrice)) {
             return -2;
         }
         
-        double $newWaterTotal = $water + $desiredWater;
+        double newWaterTotal = water + desiredWater;
         
-        if ($newWaterTotal > 100) {
+        if (newWaterTotal > 100) {
             return -3;
         }
         
-        return $desiredWater;
+        return desiredWater;
     }
     
-    public double purchaseFood(double $food, double $gold, double $desiredFood, double $foodPrice) {
-        if ($desiredFood < 0 || $desiredFood > 10){
+    public double purchaseFood(double food, double gold, double desiredFood, double foodPrice) {
+        if (desiredFood < 0 || desiredFood > 10){
             return -1;
         }
         
-        if ($gold < ($desiredFood * $foodPrice)) {
+        if (gold < (desiredFood * foodPrice)) {
             return -2;
         }
         
-        double $newFoodTotal = $food + $desiredFood;
+        double newFoodTotal = food + desiredFood;
         
-        if ($newFoodTotal > 100) {
+        if (newFoodTotal > 100) {
             return -3;
         }
         
-        return $desiredFood;
+        return desiredFood;
     }
     
-    public double purchaseArmory(double $weapons, double $gold, double $desiredWeapons, double $weaponsPrice) {
-        if ($desiredWeapons < 0 || $desiredWeapons > 10){
+    public double purchaseArmory(double weapons, double gold, double desiredWeapons, double weaponsPrice) {
+        if (desiredWeapons < 0 || desiredWeapons > 10){
             return -1;
         }
         
-        if ($gold < ($desiredWeapons * $weaponsPrice)) {
+        if (gold < (desiredWeapons * weaponsPrice)) {
             return -2;
         }
         
-        double $newWeaponsTotal = $weapons + $desiredWeapons;
+        double newWeaponsTotal = weapons + desiredWeapons;
         
-        if ($newWeaponsTotal > 100) {
+        if (newWeaponsTotal > 100) {
             return -3;
         }
         
-        return $desiredWeapons;
+        return desiredWeapons;
     }
 }
 
